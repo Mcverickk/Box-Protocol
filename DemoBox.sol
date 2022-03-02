695836e51a632e6249e75fb1bf15b094f21c10ba
Mcverickk <85528216+Mcverickk@users.noreply.github.com>
2022-03-03 02:22:45 +0530

    Update DemoBox.sol
    Added comments

--- DemoBox.sol
+++ DemoBox.sol
@@ -7,21 +7,23 @@ contract DemoBox is DemoBoxToken {
 
 
     address public thisContract = address(this);
+    // to check contracts eth balance ----not working
     uint public balance = address(this).balance;
 
+    //function to add eth to this contract and get the DemoBoxToken
     function addFundsToBox() payable public {
         uint value = msg.value;
         _mint(msg.sender, tokenAmountToMint(value));
     }
 
+    //function to withdraw eth for the DemoBoxToken
     function withdrawFunds(uint _tokenAmount) payable public {
-
         _burn(msg.sender, _tokenAmount);
         uint fundsToGive = _tokenAmount*boxTokenPrice();
         (bool sent,) = msg.sender.call{value : fundsToGive}("");
     }
 
-    //decimals and division check to be done
+    //***decimals and division check to be done
     function boxTokenPrice() private returns(uint){
 
         if(totalSupply() == 0) {
@@ -32,7 +34,8 @@ contract DemoBox is DemoBoxToken {
         }
     }
 
-    //decimals and division check to be done
+    //***decimals and division check to be done
+    //-----incomplete
     function totalBoxValue() public returns(uint256) { //have to use chainlink data feed
         return 1000;
     }
