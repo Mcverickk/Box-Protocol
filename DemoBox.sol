446b48ff11a48f0df52d4a9cd73ede42d7eaa905
Mcverickk <85528216+Mcverickk@users.noreply.github.com>
2022-03-03 02:18:22 +0530

    Update DemoBox.sol
    Added Withdraw function

--- DemoBox.sol
+++ DemoBox.sol
@@ -5,37 +5,40 @@ import "./DemoBoxToken.sol";
 
 contract DemoBox is DemoBoxToken {
 
+
+    address public thisContract = address(this);
+    uint public balance = address(this).balance;
+
     function addFundsToBox() payable public {
         uint value = msg.value;
         _mint(msg.sender, tokenAmountToMint(value));
-    } 
+    }
 
+    function withdrawFunds(uint _tokenAmount) payable public {
+
+        _burn(msg.sender, _tokenAmount);
+        uint fundsToGive = _tokenAmount*boxTokenPrice();
+        (bool sent,) = msg.sender.call{value : fundsToGive}("");
+    }
 
     //decimals and division check to be done
-    function boxTokenPrice() public returns(uint256){
+    function boxTokenPrice() private returns(uint){
+
         if(totalSupply() == 0) {
-            return 1;
+            return 1;                                          // 1token = 1 USD
         }
         else { //not correct yet
-            return (uint(totalBoxValue())/uint(totalSupply()));
+            return totalBoxValue()/totalSupply();
         }
     }
 
     //decimals and division check to be done
     function totalBoxValue() public returns(uint256) { //have to use chainlink data feed
-
+        return 1000;
     }
 
     //decimals and division check to be done
     function tokenAmountToMint(uint _value) private returns(uint) {
         return _value/boxTokenPrice();
     }
-
-
-
-
 }
-
-
-
-
