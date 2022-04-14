7e09afb30ca30f0e5699a6864841512c559f3fdc
Chirag Agarwal <85528216+Mcverickk@users.noreply.github.com>
2022-04-15 02:29:44 +0530

    Update DemoBox.sol
    ETH deposit and minting of Box Token working well with correct decimals and math.
    Task left- Calculating total Box Value and Withdrawal system

--- Contracts/DemoBox.sol
+++ Contracts/DemoBox.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.0;
 
 import "./DemoBoxToken.sol";
 import "./Swap.sol";
+import "./Price.sol";
 
-contract DemoBox is DemoBoxToken, Swap {
+contract DemoBox is DemoBoxToken, Swap, Price {
 
-    //rinkeby
+    //Made for Rinkeby Testnet
     /*
         tokenList["DAI"] = 0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735;
         tokenList["WETH9"] = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
@@ -14,10 +15,13 @@ contract DemoBox is DemoBoxToken, Swap {
         tokenList["BAT"] = 0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99;
     */
 
-    //25% USDC 50%DAI 25%BAT
+    //25% USDC 50%DAI 25%BAT in this Demo Box
 
+    //each token Balance that the box should hold
+    //******look if this needs to be made internal or private********
     mapping(string => uint256) public tokenBalance;
 
+//constructor setting the initial token balances as 0
     constructor() {
         tokenBalance["BAT"] = 0;
         tokenBalance["DAI"] = 0;
@@ -27,22 +31,30 @@ contract DemoBox is DemoBoxToken, Swap {
     }
 
 
-    //function to add eth to this contract and get the DemoBoxToken
+    //function to add ETH to this contract and get the DemoBoxToken
     function addFundsToBox() payable public {
         require(msg.value > 0, "Not enough value.");
-        tokenBalance["ETH"] = msg.value;
+        //updating the contract's ETH balance
+        tokenBalance["ETH"] += msg.value;
+        //storing msg.value locally for further use
         uint256 valueETH = msg.value;
+        //swapping ETH for WETH9(ERC-20)...will be helpfull in further token swaps
+        //using WETH9 contract for this
         swapExactETHForWETH9();
-
-        buyTokensFromWETH(tokenBalance["WETH9"]);
+        //using Uniswap to swap WETH9 for 25,25,50% for BAT,USDC and DAI respectively.
+        buyTokensFromWETH(valueETH);
+        //minting new Demo Box Tokens according to the value added in Box
         _mint(msg.sender, tokenAmountToMint(valueETH));
     }
 
+
+    //function to buy box conprising tokens in exchange for WETH9
     function buyTokensFromWETH(uint _amountWETH) internal {
+        //storing the buying amount of respective tokens in WETH
         uint buyingAmountOfBAT = _amountWETH/4;
         uint buyingAmountOfUSDC = _amountWETH/4;
         uint buyingAmountOfDAI = _amountWETH/2;
-
+        //swaping tokens and updating contract's token balances
         tokenBalance["BAT"] += _swapExactInputSingle(buyingAmountOfBAT, "WETH9", "BAT");
         tokenBalance["WETH9"] -= buyingAmountOfBAT;
         tokenBalance["USDC"] += _swapExactInputSingle(buyingAmountOfUSDC, "WETH9", "USDC");
@@ -51,43 +63,60 @@ contract DemoBox is DemoBoxToken, Swap {
         tokenBalance["WETH9"] -= buyingAmountOfDAI;
     }
 
+
+
+
     //function to withdraw eth for the DemoBoxToken
+    //*************to we worked on*************
     function withdrawFunds(uint _tokenAmount) payable public {
         _burn(msg.sender, _tokenAmount);
-        uint fundsToGive = _tokenAmount*boxTokenPrice();
+        uint fundsToGive = _tokenAmount*getBoxTokenPrice();
         (bool sent,) = msg.sender.call{value : fundsToGive}("");
     }
 
-
-    //***decimals and division check to be done
-    function boxTokenPrice() private returns(uint){
-
-        if(totalSupply() == 0) {
-            return 1;                                          // 1token = 1 USD
-        }
-        else { //not correct yet
-            return totalBoxValue()/totalSupply();
-        }
-    }
-
-    //***decimals and division check to be done
-    //-----incomplete
-    function totalBoxValue() public returns(uint256) { //have to use chainlink data feed
-        return 1000;
-    }
-
-    //decimals and division check to be done
+    //function to get the amount of Box tokens to be minted
     function tokenAmountToMint(uint _value) private returns(uint) {
-        return _value/boxTokenPrice();
+        //calculating the added ETH value in USD with 18 decimals
+        uint valueInUSD =  _value*uint256(getPrice(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e))/uint256(10**8);
+        //getting the price of Box Token in USD with 18 decimals
+        uint boxTokenPrice = getBoxTokenPrice(); 
+        //returning the amount of box tokens to be minted in 18 decimals
+        return (valueInUSD*(10**18)/boxTokenPrice);
     }
+
+    //function to get the price of each Box Token
+    function getBoxTokenPrice() private returns(uint){
+        //if it is the 1st deposit...Box token price = 1 USD 
+        if(totalSupply() == 0) {
+            // returing 1 USD in 18 decimals
+            return uint(10**18);                                          
+        }
+        //calculating Box Token price if it is not the 1st deposit
+        else { 
+            //getting total Box value and dividing it by total supply to get price in 18 decimals
+            return getTotalBoxValue()*(10**18)/totalSupply(); 
+        }
+    }
+
+
+    //********************get price in USD and 18 decimals*************************
+    //************Have to use Chainlink prices to calculate total value*************
+    function getTotalBoxValue() public returns(uint256) { 
+        //returning 1000USD in 18 decimals as total Box Value
+        return 1000*(10**18);
+    }
+
+    
 
 
     //function to swap exact ETH to WETH9
     function swapExactETHForWETH9() payable public {
         require(msg.value > 0, "Not enough value.");
+        //calling the deposit function in WETH9 contract to wrap ETH
         wethtoken.deposit{value: msg.value}();
-        tokenBalance["WETH9"] = msg.value;
+        //updating contract's token balance
         tokenBalance["ETH"] -= msg.value;
+        tokenBalance["WETH9"] += msg.value;    
     }
 }
 
